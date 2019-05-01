b40e31da300a955b28c1ef0870decddda49187a2
gg <jgroc-de@student.42.fr>
2019-05-01 19:09:03 +0200

    maj

--- install.sh
+++ install.sh
@@ -1,23 +1,31 @@
-#sudo add-apt-repository "deb http://www.geogebra.net/linux/ stable main"
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C072A32983A736CF
-sudo apt update
-sudo apt upgrade
-sudo apt install git vim vlc audacity gimp filezilla chromium-browser steam mysql-server mysql-client gnome-tweak-tool steam geogebra-classic htop
-sudo apt install gcc make xorg-dev libx11-dev
-sudo apt install python3 pip3
-sudo apt install php composer php-gd php-xml php-zip php-curl php-intl php-json php-mbstring php-mysql php-xml php-zip
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install vim vlc audacity gimp filezilla chromium-browser steam mysql-server mysql-client gnome-tweak-tool steam htop
+sudo apt -y install gcc make xorg-dev libx11-dev
+sudo apt -y install python3 pip3
+sudo apt -y install php composer php-gd php-xml php-zip php-curl php-intl php-json php-mbstring php-mysql php-xml php-zip
 sudo snap install slack --classic
 sudo snap install skype --classic
 sudo snap install spotify --classic
+sudo snap install postman --classic
+#geogebra
+#sudo add-apt-repository "deb http://www.geogebra.net/linux/ stable main"
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C072A32983A736CF
+#sudo apt install geogebra-classic
+sudo apt -y autoremove
 #Vim plugin
-git clone https://github.com/jgroc-de/.vim.git
-cd .vim
+rm -rf ~/.vim
+cp inputrc ~/.inputrc
+cp bashrc ~/.bashrc
+mkdir -p ~/.vim/{autoload,colors,syntax,plugin,spell,config,bundle}
+cp -R config ~/.vim/
+cp vimrc ~/.vim/vimrc
+cd ~/.vim
+ln -s vimrc ~/.vimrc
 git clone https://github.com/tpope/vim-pathogen.git pathogen
 cd autoload
 ln -s ../pathogen/autoload/pathogen.vim .
-cd ..
-mkdir -p bundle
-cd bundle
+cd ../bundle
 git clone https://github.com/scrooloose/nerdtree.git nerdtree
 git clone https://github.com/othree/html5.vim
 git clone https://github.com/vim-airline/vim-airline.git
@@ -31,5 +39,9 @@ git clone https://github.com/hail2u/vim-css3-syntax.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/evidens/vim-twig.git
 git clone https://github.com/justinmk/vim-syntax-extra.git
-git clone https://github.com/ludovicchabant/vim-gutentags.git
+#git clone https://github.com/ludovicchabant/vim-gutentags.git
 git clone https://github.com/ctrlpvim/ctrlp.vim.git
+#mode powersave ba default
+#sudo apt-get install cpufrequtils
+#echo 'GOVERNOR="powersave"' | sudo tee /etc/default/cpufrequtils
+#sudo systemctl disable ondemand
