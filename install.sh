bf58ef091432491d58911b1d4aea21b4ccae6bca
jgroc-de <jgroc-de@student.42.fr>
2019-05-26 18:33:42 +0200

    node + curl update

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt -y install vim vlc audacity gimp filezilla chromium-browser steam mysql-server mysql-client gnome-tweak-tool steam htop
+sudo apt -y update && sudo apt -y upgrade
+sudo apt -y install vim vlc audacity gimp filezilla chromium-browser steam mysql-server mysql-client gnome-tweak-tool steam htop curl
 sudo apt -y install gcc make xorg-dev libx11-dev
 sudo apt -y install python3 pip3 mycli
-#manque node
 sudo apt -y install php composer php-gd php-xml php-zip php-curl php-intl php-json php-mbstring php-mysql php-xml php-zip
+#last node lts
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt -y install nodejs npm
 sudo snap install slack --classic
-#sudo snap install skype --classic
 sudo snap install spotify --classic
 sudo snap install postman --classic
 
