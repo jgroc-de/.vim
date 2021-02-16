c7d389e77e448249b49795a58c25e3b420fdbcd6
jgroc-de <jerome.gds@gmail.com>
2021-02-16 14:38:52 +0100

    update audacious + ssh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt -y update && sudo apt -y upgrade
 sudo apt -y install vim vlc filezilla mysql-server mysql-client htop curl #terminator
-sudo apt -y install audacity gimp
+sudo apt -y install audacity audacious #gimp
 #sudo apt -y install steam chromium-browser gnome-tweak-tool
 sudo apt -y install gcc make xorg-dev libx11-dev
 sudo apt -y install python3 pip3 mycli
@@ -14,7 +14,7 @@ sudo apt -y install php composer php-gd php-xml php-zip php-curl php-intl php-js
 #sudo snap install spotify --classic
 #sudo snap install postman --classic
 #sudo snap install phpstorm --classic
-sudo snap install code --classic
+#sudo snap install code --classic
 
 #geogebra
 #sudo add-apt-repository "deb http://www.geogebra.net/linux/ stable main"
@@ -28,4 +28,4 @@ cp inputrc ~/.inputrc
 cp bashrc ~/.bashrc
 
 ./vim.sh
-./ssh_git.sh
+#./ssh_git.sh
